ec903c23f59b95541f7e9cd8413c6b63c4aa0a94
Chathura <lpcb@Chathuras-MacBook-Pro-2.local>
2024-07-05 17:13:40 +0530

    eslinting changes p1

--- payment_sdk/babel.config.js
+++ payment_sdk/babel.config.js
@@ -5,4 +5,20 @@ module.exports = {
     "@babel/preset-typescript",
     "module:@react-native/babel-preset",
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@services": "./src/services",
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@util": "./src/util",
+          "@context": "./src/context"
+        }
+      }
+    ]
+  ]
 };
