1997b97131afb6979cd78c699c6bdc9f186b460d
Chathura <lpcb@Chathuras-MacBook-Pro-2.local>
2024-07-15 21:18:26 +0530

    remove unwanted pkgs and configs

--- payment_sdk/setupTests.js
+++ payment_sdk/setupTests.js
@@ -1,15 +1,15 @@
 global.__DEV__ = process.env.NODE_ENV !== "production";
 
-jest.mock('react-native-vision-camera', () => ({
-    Camera: 'mockCamera',
-    useCameraDevices: jest.fn(),
-    useFrameProcessor: jest.fn(),
-  }));
+// jest.mock('react-native-vision-camera', () => ({
+//     Camera: 'mockCamera',
+//     useCameraDevices: jest.fn(),
+//     useFrameProcessor: jest.fn(),
+//   }));
   
-  jest.mock('react-native-vision-camera-text-recognition', () => ({
-    useTextRecognition: jest.fn(),
-  }));
+//   jest.mock('react-native-vision-camera-text-recognition', () => ({
+//     useTextRecognition: jest.fn(),
+//   }));
   
-  jest.mock('react-native-worklets-core', () => ({
-    Worklets: jest.fn(),
-  }));
+//   jest.mock('react-native-worklets-core', () => ({
+//     Worklets: jest.fn(),
+//   }));
