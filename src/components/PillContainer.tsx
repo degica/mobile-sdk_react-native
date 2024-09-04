88f096b24cf9ac66607f50ee14533e3e71ac4ddb
Tharindu Kumarasiri <ku.tharindu@gmail.com>
2024-09-04 16:26:07 +0530

    Replace with figma icons

--- src/components/PillContainer.tsx
+++ src/components/PillContainer.tsx
@@ -1,27 +1,42 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import { StyleSheet, View, Image, FlatList } from "react-native";
+import { StyleSheet, View, Image, FlatList } from 'react-native';
 
-import { StateContext } from "../context/state";
+import { StateContext } from '../context/state';
 
-import { PaymentType, sessionShowPaymentMethodType } from "../util/types";
+import { PaymentType, sessionShowPaymentMethodType } from '../util/types';
 
-import PaymentMethodImages from "../assets/images/paymentMethodImages";
+import PaymentMethodImages from '../assets/images/paymentMethodImages';
 
-import { responsiveScale } from "../theme/scalling";
+import { responsiveScale } from '../theme/scalling';
 
-import Pill from "./Pill";
+import Pill from './Pill';
 
 type Props = {
   onSelect: (type: PaymentType) => void;
   selectedItem: PaymentType;
 };
 
+const squareSizeImages = [
+  PaymentType.CREDIT,
+  PaymentType.KONBINI,
+  PaymentType.PAY_PAY,
+];
+
 const PillContainer = ({ onSelect, selectedItem }: Props) => {
   const { paymentMethods } = useContext(StateContext);
 
   const getIcon = (slug: PaymentType) => {
-    return <Image source={PaymentMethodImages[slug]} style={styles.image} />;
+    return (
+      <Image
+        source={PaymentMethodImages[slug]}
+        style={[
+          styles.image,
+          squareSizeImages.includes(slug) ? styles.squareImage : {},
+        ]}
+        resizeMode="contain"
+      />
+    );
   };
 
   const renderItem = ({ item }: { item: sessionShowPaymentMethodType }) => {
@@ -57,8 +72,11 @@ const styles = StyleSheet.create({
   },
   image: {
     width: responsiveScale(38),
-    height: responsiveScale(24),
-    resizeMode: "contain",
+    height: responsiveScale(32),
+  },
+  squareImage: {
+    width: responsiveScale(32),
+    height: responsiveScale(32),
   },
 });
 
