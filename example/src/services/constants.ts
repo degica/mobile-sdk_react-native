b6adafad3038dc66f8962e0b4f00f9586f746991
amnix <amantonkem@gmail.com>
2024-10-21 20:52:34 +0900

    Changed example server URL

--- example/src/services/constants.ts
+++ example/src/services/constants.ts
@@ -1,3 +1,3 @@
-export const PUBLISHABLE_KEY_URL = 'https://rn-komoju-app.glitch.me/serve-keys';
+export const PUBLISHABLE_KEY_URL = 'https://komoju-3pqqmesoka-an.a.run.app/serve-key';
 export const CREATE_SESSION_URL =
-  'https://rn-komoju-app.glitch.me/create-session';
+  'https://komoju-3pqqmesoka-an.a.run.app/create-session';
